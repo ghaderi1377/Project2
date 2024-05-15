2a2384b6a12e706871d2e0c17047b8bbe5a2707c
Rashed Ghaderi <rashedghaderi771@gmail.com>
2024-05-15 18:24:36 +0330

    15-05-2024

--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
-
-      var powFlag = false;
-      function addToCalculator(value) {
-        document.getElementById("displayResult").value += value;
-        if (powFlag) {
-          var powNumbers = document
-            .getElementById("displayResult")
-            .value.split("^");
-          document.getElementById("displayResult").value = Math.pow(
-            powNumbers[0],
-            powNumbers[1]
-          );
-          powFlag = false;
-        }
+let powFlag = false;
+let result1 = document.getElementById("displayResult");
+function addToCalculator(value) {
+  result1.value += value;
+  if (powFlag) {
+    let powNumbers = result1.value.split("^");
+    result1.value = Math.pow(powNumbers[0], powNumbers[1]);
+    powFlag = false;
+  }
+}
+function addToCalculator1(value) {
+  if (result1.value != "") {
+    result1.value += value;
+  }
+}
+function addToCalculator2(value) {
+  if (result1.value != "0") {
+    result1.value += value;
+  }
+}
+function addToCalculator3(value) {
+  if (result1.value == "") {
+    result1.value = result1.value + "0" + value;
+    return;
+  }
+  result1.value += value;
+}
+function reset() {
+  result1.value = "";
+}
+function result() {
+  result1.value = eval(result1.value);
+}
+function mathCalculator(mathFunc) {
+    if (result1.value != "") {
+      if (mathFunc == "pow") {
+        powFlag = true;
+        result1.value += "^";
+      } else {
+        result1.value = Math[mathFunc](result1.value);
       }
-      function reset() {
-        document.getElementById("displayResult").value = "";
-      }
-      function result() {
-        document.getElementById("displayResult").value = eval(
-          document.getElementById("displayResult").value
-        );
-      }
-      function mathCalculator(mathFunc) {
-        if (mathFunc == "pow") {
-          powFlag = true;
-          document.getElementById("displayResult").value += "^";
-        } else {
-          document.getElementById("displayResult").value = Math[mathFunc](
-            document.getElementById("displayResult").value
-          );
-        }
-      }
+    }
+}
